3b867141ac92837a5a4eb504a8ab358a6ccbed95
Tony Wang <tony94042@gmail.com>
2023-01-03 13:50:56 +1100

    Added Some Basic Collision Code

--- script.js
+++ script.js
@@ -97,6 +97,8 @@ window.addEventListener('load', function(){
       this.x = this.game.width;
       this.speedX = Math.random() * -1.5 - 0.5;
       this.markedForDeletion = false;
+      this.lives = 5;
+      this.score = this.lives;
     }
     update(){
       this.x += this.speedX;
@@ -106,6 +108,9 @@ window.addEventListener('load', function(){
     draw(context){
       context.fillStyle = 'red';
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.fillStyle = 'black'
+      context.font = '20px Helvetica'
+      context.fillText(this.lives, this.x, this.y);
     }
   }
 
@@ -171,6 +176,19 @@ window.addEventListener('load', function(){
           }
           this.enemies.forEach(enemy => {
             enemy.update();
+            if (this.checkCollision(this.player, enemy)){
+              enemy.markedForDeletion = true;
+            }
+            this.player.projectiles.forEach(projectile => {
+              if(this.checkCollision(projectile, enemy)){
+                enemy.lives--;
+                projectile.markedForDeletion = true;
+                if (enemy.lives <= 0){
+                  enemy.markedForDeletion = true;
+                  this.score += enemy.score;
+                }
+              }
+            });
           });
           this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
           if (this.enemyTimer > this.enemyInterval && !this.gameOver){
@@ -179,7 +197,6 @@ window.addEventListener('load', function(){
           } else {
             this.enemyTimer += deltaTime;
           }
-
       }
 
       draw(context) {
@@ -189,12 +206,18 @@ window.addEventListener('load', function(){
             enemy.draw(context);
           })
       }
+
       addEnemy(){
         this.enemies.push(new Angler1(this));
-        console.log(this.enemies);
       }
-  }
 
+      checkCollision(rect1, rect2){
+        return ( rect1.x < rect2.x + rect2.width &&
+                 rect1.x + rect1.width > rect2.x &&
+                 rect1.y < rect2.y + rect2.height &&
+                 rect1.height + rect1.y > rect2.y)
+  }
+}
   const game = new Game(canvas.width, canvas.height);
   let lastTime = 0;
 
