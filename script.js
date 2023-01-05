5cff326291fa76ca6db3b2a150dc7592d8253ed4
Tony Wang <tony94042@gmail.com>
2023-01-05 14:14:17 +1100

    Started Particles

--- script.js
+++ script.js
@@ -49,7 +49,27 @@ window.addEventListener('load', function(){
   }
 
   class Particle {
+    constructor(game, x, y){
+      this.game = game;
+      this.x = x;
+      this.y = y;
+      this.image = document.getElementById('gears');
+      this.frameX = Math.floor(Math.random() * 3);
+      this.framey = Math.floor(Math.random() * 3);
+      this.spriteSize = 50;
+      this.sizeModifier = (Math.random() * 0.5 + 0.5).toFixed(1);
+      this.size = this.spriteSize * this.sizeModifier;
+      this.speedX = Math.random() * 6 - 3;
+      this.speeY = Math.random() * -15;
+      this.gravity = 0.5;
+      this.markedForDeletion = false;
+      this.angle = 0;
+      //speed of gear rotation
+      this.va = Math.random() * 0.2 - 0.1;
+    }
+    update(){
 
+    }
   }
 
   class Player {
